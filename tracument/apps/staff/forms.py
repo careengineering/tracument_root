a8d1ebc63b3ccd3a61a58446485f749aeb2916a9
careengineering <careengineeringx@gmail.com>
2024-06-10 21:13:26 +0300

    staff

--- tracument/apps/staff/forms.py
+++ tracument/apps/staff/forms.py
@@ -1,7 +1,21 @@
 from django import forms
-from .models import Staff
+from .models import Staff,Unit, Title, Payroll, Duty
 
 class StaffForm(forms.ModelForm):
     class Meta:
         model = Staff
         fields = ['name', 'surname', 'payroll', 'unit', 'title', 'duty', 'is_active']
+
+class StaffFilterForm(forms.Form):
+    q = forms.CharField(label='Genel Arama', required=False)
+    name = forms.CharField(label='Ad', required=False)
+    surname = forms.CharField(label='Soyad', required=False)
+    unit = forms.ModelChoiceField(queryset=Unit.objects.all(), label='Birim', required=False)
+    title = forms.ModelChoiceField(queryset=Title.objects.all(), label='Unvan', required=False)
+    payroll = forms.ModelChoiceField(queryset=Payroll.objects.all(), label='Kadro', required=False)
+    duty = forms.ModelChoiceField(queryset=Duty.objects.all(), label='Görev', required=False)
+    is_active = forms.ChoiceField(
+        choices=[('', 'Tümü'), (True, 'Aktif'), (False, 'Pasif')],
+        label='Durum',
+        required=False
+    )
--- tracument/apps/staff/urls.py
+++ tracument/apps/staff/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.staff_list, name='staff-list'),
-    path('create/', views.staff_create, name='staff-create'),
-    path('update/<int:pk>/', views.staff_update, name='staff-update'),
-    path('delete/<int:pk>/', views.staff_delete, name='staff-delete'),
+    path('staff/', views.staff_list, name='staff-list'),
+    path('staff/create/', views.staff_create, name='staff-create'),
+    path('staff/update/<int:pk>/', views.staff_update, name='staff-update'),
+    path('staff/delete/<int:pk>/', views.staff_delete, name='staff-delete'),
     path('staff-by-unit', views.staff_by_unit, name="staff-by-unit"),
     path('printable-staff-by-unit/', views.printable_staff_by_unit, name="printable-staff-by-unit"),
 ]
