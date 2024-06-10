c2d442a94b14c75d24d1e83cf2c2b1b8fc4eeba4
careengineering <careengineeringx@gmail.com>
2024-06-10 17:37:45 +0300

    staff

--- tracument/apps/staff/urls.py
+++ tracument/apps/staff/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.staff_list, name='staff-list'),
+    path('create/', views.staff_create, name='staff-create'),
+    path('update/<int:pk>/', views.staff_update, name='staff-update'),
+    path('delete/<int:pk>/', views.staff_delete, name='staff-delete'),
     path('staff-by-unit', views.staff_by_unit, name="staff-by-unit"),
-    path('staff-list', views.staff_list, name="staff-list"),
+    path('printable-staff-by-unit/', views.printable_staff_by_unit, name="printable-staff-by-unit"),
 ]
--- tracument/apps/staff/views.py
+++ tracument/apps/staff/views.py
@@ -1,12 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from .models import Staff, Unit
+from .forms import StaffForm
 
-# - Staff list
+# Staff List
 def staff_list(request):
-    staffs = Staff.objects.all().select_related('unit','payroll').prefetch_related('duty')
-    context = {'stafFs':staffs}
-    return render(request,'staff/staff_list.html', context=context)
+    staffs = Staff.objects.all().select_related('unit', 'payroll').prefetch_related('duty')
+    context = {'staffs': staffs}
+    return render(request, 'staff/staff_list.html', context)
 
+# Create Staff
+def staff_create(request):
+    if request.method == 'POST':
+        form = StaffForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('staff-list')
+    else:
+        form = StaffForm()
+    return render(request, 'staff/staff_form.html', {'form': form})
+
+# Update Staff
+def staff_update(request, pk):
+    staff = get_object_or_404(Staff, pk=pk)
+    if request.method == 'POST':
+        form = StaffForm(request.POST, instance=staff)
+        if form.is_valid():
+            form.save()
+            return redirect('staff-list')
+    else:
+        form = StaffForm(instance=staff)
+    return render(request, 'staff/staff_form.html', {'form': form})
+
+# Delete Staff
+def staff_delete(request, pk):
+    staff = get_object_or_404(Staff, pk=pk)
+    if request.method == 'POST':
+        staff.delete()
+        return redirect('staff-list')
+    return render(request, 'staff/staff_confirm_delete.html', {'staff': staff})
+
+# Staff List by Unit
 def staff_by_unit(request):
     units = Unit.objects.all()
     unit_staffs = {}
@@ -15,3 +49,15 @@ def staff_by_unit(request):
         unit_staffs[unit] = staff
     context = {'unit_staffs': unit_staffs}
     return render(request, 'staff/staff-by-unit.html', context)
+
+# Staff List by Unit - printable
+def printable_staff_by_unit(request):
+    units = Unit.objects.all()
+    unit_staffs = {}
+    for unit in units:
+        staff = Staff.objects.filter(unit=unit).select_related('title').prefetch_related('duty').order_by('-title__name')
+        unit_staffs[unit] = staff
+    context = {'unit_staffs': unit_staffs}
+    return render(request, 'staff/printable-staff-by-unit.html', context)
+
+
